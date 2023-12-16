66f283d7c1ee535521412b59faccaf9160e5d10a
dhoang21 <datxuan.hoang@dxc.com>
2023-12-16 18:52:06 +0700

    added login and database connection

---
+++ database.py
@@ -0,0 +1,29 @@
+import pyodbc
+
+
+class Database:
+    def __init__(self, drive='SQL Server', server='localhost', database='duan1_final', username='sa',
+                 password='P@ssword123456'):
+        self.drive = drive
+        self.server = server
+        self.database = database
+        self.username = username
+        self.password = password
+
+        # creating database connection
+        connection_string = (
+            f"DRIVER={self.drive};"
+            f"SERVER={self.server};"
+            f"DATABASE={self.database};"
+            f"UID={self.username};"
+            f"PWD={self.password};"
+        )
+        self.con = pyodbc.connect(connection_string)
+        self.cur = self.con.cursor()
+
+    def login(self, username, password):
+        sql_select_query = "SELECT * FROM NHANVIEN WHERE MANV=? AND MATKHAU=?"
+        self.cur.execute(sql_select_query, (username, password))
+        result = self.cur.fetchall()
+        print(result)
+        return result
--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+from tkinter import *
+import login
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#main method
+def main():
+    root = Tk()
+    root.title("Quản lý cửa hàng")
+    root.geometry("1200x930+50+50")
+    root.resizable(False, True)
 
+    #Parsing the root window to the Login class
+    #Initiating the System
+    login.Login(root)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    root.mainloop()
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
